06257a4fc910c1dc2297b32ace6ac44e4d38fa74
JARD-PC\Jard <Jard.wesdorp@hotmail.com>
2025-07-10 16:38:46 +0200

    Init function in other files

---
+++ Core/Inc/init.h
@@ -0,0 +1,13 @@
+//
+// Created by Jard on 10-7-2025.
+//
+
+#ifndef FREERTOS_STM32_INIT_H
+#define FREERTOS_STM32_INIT_H
+
+#include "main.h"
+
+void SystemClock_Config(void);
+void MX_GPIO_Init(void);
+
+#endif //FREERTOS_STM32_INIT_H
---
+++ Core/Inc/uart.h
@@ -0,0 +1,16 @@
+//
+// Created by Jard on 10-7-2025.
+//
+
+#ifndef FREERTOS_STM32_UART_H
+#define FREERTOS_STM32_UART_H
+
+#include "init.h"
+#include "stm32f4xx_hal_uart.h"
+
+void MX_USART2_UART_Init(void);
+
+
+
+
+#endif //FREERTOS_STM32_UART_H
